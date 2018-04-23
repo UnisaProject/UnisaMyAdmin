0a22c39578aa19dbaa316e471a781f05e4cce5ec
Charl Thiem <charl@opencollab.co.za>
2018-04-23 12:40:06 +0200

    UMA-1 etag caching

--- myadmin-webapps/exam-timetable-webapp/src/main/java/za/ac/unisa/myadmin/ExamTimetableApplication.java
+++ myadmin-webapps/exam-timetable-webapp/src/main/java/za/ac/unisa/myadmin/ExamTimetableApplication.java
@@ -4,9 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
 public class ExamTimetableApplication extends SpringBootServletInitializer {
+
+	@Bean
+	public Filter shallowETagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
+	}
 
 	// By extending SpringBootServletInitializer and override the configure method
 	// so that the Spring application sources can be configured by
