78dab44443d02f71fc88bac1472ff69c5c55de3c
Charl Thiem <charl@opencollab.co.za>
2018-05-03 09:53:48 +0200

    UMA-11 Better proxy compatibilty

--- myadmin-angular/exam-timetable-app/src/app/services/exam-admission.service.ts
+++ myadmin-angular/exam-timetable-app/src/app/services/exam-admission.service.ts
@@ -12,25 +12,25 @@ export class ExamAdmissionService {
   }
 
   getExamAdmissions(): Observable<ExamAdmissionInfo[]> {
-    return this.http.get<ExamAdmissionInfo[]>('/rest/examadmissions');
+    return this.http.get<ExamAdmissionInfo[]>('rest/examadmissions');
   }
 
   getExamAdmissionsByYear(year: number): Observable<ExamAdmissionInfo[]> {
     const params = new HttpParams().set('year', <string><any>year);
 
-    return this.http.get<ExamAdmissionInfo[]>('/rest/examadmissions', { params });
+    return this.http.get<ExamAdmissionInfo[]>('rest/examadmissions', { params });
   }
 
   getExamAdmissionsByExamPeriodCode(examPeriodCode: number): Observable<ExamAdmissionInfo[]> {
     const params = new HttpParams().set('examPeriodCode', <string><any>examPeriodCode);
 
-    return this.http.get<ExamAdmissionInfo[]>('/rest/examadmissions', { params });
+    return this.http.get<ExamAdmissionInfo[]>('rest/examadmissions', { params });
   }
 
   getExamAdmissionsByExamType(examType: number): Observable<ExamAdmissionInfo[]> {
     const params = new HttpParams().set('examType', <string><any>examType);
 
-    return this.http.get<ExamAdmissionInfo[]>('/rest/examadmissions', { params });
+    return this.http.get<ExamAdmissionInfo[]>('rest/examadmissions', { params });
   }
 
   getExamAdmissionsByYearAndExamPeriodCode(year: number, examPeriodCode: number): Observable<ExamAdmissionInfo[]> {
@@ -38,7 +38,7 @@ export class ExamAdmissionService {
       .set('year', <string><any>year)
       .set('examPeriodCode', <string><any>examPeriodCode);
 
-    return this.http.get<ExamAdmissionInfo[]>('/rest/examadmissions', { params });
+    return this.http.get<ExamAdmissionInfo[]>('rest/examadmissions', { params });
   }
 
   getExamAdmissionsByYearAndExamType(year: number, examType: number): Observable<ExamAdmissionInfo[]> {
@@ -46,7 +46,7 @@ export class ExamAdmissionService {
       .set('year', <string><any>year)
       .set('examType', <string><any>examType);
 
-    return this.http.get<ExamAdmissionInfo[]>('/rest/examadmissions', { params });
+    return this.http.get<ExamAdmissionInfo[]>('rest/examadmissions', { params });
   }
 
   getExamAdmissionsByExamPeriodCodeAndExamType(examPeriodCode: number, examType: number): Observable<ExamAdmissionInfo[]> {
@@ -54,7 +54,7 @@ export class ExamAdmissionService {
       .set('examPeriodCode', <string><any>examPeriodCode)
       .set('examType', <string><any>examType);
 
-    return this.http.get<ExamAdmissionInfo[]>('/rest/examadmissions', { params });
+    return this.http.get<ExamAdmissionInfo[]>('rest/examadmissions', { params });
   }
 
   getExamAdmissionsByYearAndExamPeriodCodeAndExamType(year: number, examPeriodCode: number, examType: number): Observable<ExamAdmissionInfo[]> {
@@ -63,6 +63,6 @@ export class ExamAdmissionService {
       .set('examPeriodCode', <string><any>examPeriodCode)
       .set('examType', <string><any>examType);
 
-    return this.http.get<ExamAdmissionInfo[]>('/rest/examadmissions', { params });
+    return this.http.get<ExamAdmissionInfo[]>('rest/examadmissions', { params });
   }
 }
