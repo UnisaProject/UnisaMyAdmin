5cd070cacb1c80ece05d2b665753a06d411a7767
Charl Thiem <charl.thiem@gmail.com>
2018-05-11 17:11:09 +0200

    WIP unit test datasource
    WIP unit test datasource

--- myadmin-service-impls/study-quotation-service/src/test/java/za/ac/unisa/myadmin/service/studyquotation/date/impl/StudyQuoteDateServiceImplTest.java
+++ myadmin-service-impls/study-quotation-service/src/test/java/za/ac/unisa/myadmin/service/studyquotation/date/impl/StudyQuoteDateServiceImplTest.java
@@ -1,5 +1,16 @@
 package za.ac.unisa.myadmin.service.studyquotation.date.impl;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import za.ac.unisa.myadmin.service.studyquotation.date.dao.StudyQuoteDateRepository;
+
+@PropertySource("classpath:application.properties")
+@RunWith(SpringRunner.class)
+@ComponentScan(basePackageClasses = {StudyQuoteDateRepository.class})
+@DataJpaTest
 public class StudyQuoteDateServiceImplTest {
 
 }
