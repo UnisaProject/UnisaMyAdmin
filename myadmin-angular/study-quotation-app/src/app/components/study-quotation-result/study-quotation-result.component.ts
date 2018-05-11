c154e64082a40f9f41713192cc8a88169220b6b1
adrianhfoster <adrian@opencollab.co.za>
2018-05-11 14:24:42 +0200

    UMA-16 (studyquote) Show message on quote screen when there are errors

--- myadmin-angular/study-quotation-app/src/app/components/study-quotation-result/study-quotation-result.component.ts
+++ myadmin-angular/study-quotation-app/src/app/components/study-quotation-result/study-quotation-result.component.ts
@@ -3,6 +3,7 @@ import {BlockUI, NgBlockUI} from "ng-block-ui";
 import {StudyFeeCriteriaService, StudyFeeQuotationService} from '../../services';
 import {StudyQuotationRequest} from '../../info-objects';
 import {StudyQuotation} from "../../info-objects/study-quotation";
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-study-quotation-result',
@@ -12,24 +13,29 @@ import {StudyQuotation} from "../../info-objects/study-quotation";
 export class StudyQuotationResultComponent implements OnInit {
 
   public studyQuotationInfo:StudyQuotation;
+  public errorMessage:string;
 
   @BlockUI()
-  private blockUI: NgBlockUI;
+  private blockUI:NgBlockUI;
 
-  constructor(private studyFeeCriteriaService: StudyFeeCriteriaService,
-              private studyFeeQuotationService: StudyFeeQuotationService) { }
+  constructor(private studyFeeCriteriaService:StudyFeeCriteriaService,
+              private studyFeeQuotationService:StudyFeeQuotationService) {
+  }
 
   ngOnInit() {
     this.calculateStudyQuotation(this.studyFeeCriteriaService.searchCriteria);
   }
 
-  private calculateStudyQuotation(searchCriteria: StudyQuotationRequest): void {
+  private calculateStudyQuotation(searchCriteria:StudyQuotationRequest):void {
     this.studyFeeQuotationService.calculateStudyQuotation(searchCriteria)
-      .subscribe((studyQuotationInfo: StudyQuotation) => {
+      .subscribe((studyQuotationInfo:StudyQuotation) => {
           this.studyQuotationInfo = studyQuotationInfo;
         },
         error => {
-          console.log(error);
+          if (error instanceof HttpErrorResponse) {
+            console.log(error);
+            this.errorMessage = error.message;
+          }
           this.blockUI.stop();
         },
         () => {
