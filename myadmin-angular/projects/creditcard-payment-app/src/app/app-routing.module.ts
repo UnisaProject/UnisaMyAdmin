f1595d5c85bdcbdd0e916bb81efb2532ba0b3f79
Charl Thiem <charl.thiem@gmail.com>
2018-06-01 15:14:46 +0200

    UMA-44 Search screen

--- myadmin-angular/projects/creditcard-payment-app/src/app/app-routing.module.ts
+++ myadmin-angular/projects/creditcard-payment-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PaymentSearchComponentComponent} from "./components/payment-search-component/payment-search-component.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/search', pathMatch: 'full'},
+  { path: 'search', component: PaymentSearchComponentComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- myadmin-angular/projects/creditcard-payment-app/src/app/app.module.ts
+++ myadmin-angular/projects/creditcard-payment-app/src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ClarityModule} from '@clr/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlockUIModule} from "ng-block-ui";
+
+import '@webcomponents/custom-elements';
+import '@clr/icons';
+import '@clr/icons/shapes/all-shapes';
+import {MyadminLibModule, HttpErrorInterceptor} from "myadmin-lib";
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { PaymentSearchComponentComponent } from './components/payment-search-component/payment-search-component.component';
+/** Http interceptor providers in outside-in order */
+export const HttpInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentSearchComponentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MyadminLibModule,
+    ClarityModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BlockUIModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    HttpInterceptorProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
