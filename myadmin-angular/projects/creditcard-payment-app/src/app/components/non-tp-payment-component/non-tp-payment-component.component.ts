6b5674faa9bb8a8ec4171f9cbc5a2bebcaf91eed
Charl Thiem <charl@opencollab.co.za>
2018-06-04 22:55:22 +0200

    UMA-45 nontp screen input

--- myadmin-angular/projects/creditcard-payment-app/src/app/components/non-tp-payment-component/non-tp-payment-component.component.ts
+++ myadmin-angular/projects/creditcard-payment-app/src/app/components/non-tp-payment-component/non-tp-payment-component.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {CreditCardPaymentForm} from "../../info-objects";
+import {CreditCardFormService} from "../../services/creditcard-form.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'unisa-non-tp-payment-component',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NonTpPaymentComponentComponent implements OnInit {
 
-  constructor() { }
+  nonTpForm: FormGroup;
+  creditCardPaymentForm: CreditCardPaymentForm;
+
+  constructor(private router:Router,
+              private formBuilder: FormBuilder,
+              private creditCardFormService: CreditCardFormService) {
+    this.initForm();
+  }
+
+  private initForm(){
+    this.nonTpForm = this.formBuilder.group({
+
+    });
+  }
 
   ngOnInit() {
+    this.creditCardPaymentForm = this.creditCardFormService.creditCardPaymentForm;
+    if(this.creditCardPaymentForm === null ||  this.creditCardPaymentForm.studentNumber === null){
+      this.router.navigateByUrl("/studentInput")
+    }
+    else {
+      // TODO fake some data for now
+      this.creditCardPaymentForm.studentName = "John Doe";
+      this.creditCardPaymentForm.regStatusDescription = "Registered";
+      this.nonTpForm.patchValue({...this.creditCardPaymentForm});
+    }
   }
 
 }
--- myadmin-angular/projects/creditcard-payment-app/src/app/info-objects/credit-card-payment-form.ts
+++ myadmin-angular/projects/creditcard-payment-app/src/app/info-objects/credit-card-payment-form.ts
@@ -1,7 +1,9 @@
 export interface CreditCardPaymentForm{
   studentNumber: string;
+  studentName?:string;
 
   // Qualification object in java
   qualDesc?: string;
   qualCode?: string;
+  regStatusDescription?:string;
 }
