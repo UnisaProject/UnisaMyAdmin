b6a672692d681b5123788b6b0e51aeb78f39fcfb
adrianhfoster <adrian@opencollab.co.za>
2018-07-30 17:18:09 +0200

    UMA-57 Academic Record App : Angular screens

---
+++ myadmin-angular/projects/academic-history-record-app/src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { BlockUIModule} from "ng-block-ui";
+import { ClarityModule } from '@clr/angular';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { AcadHistoryInputComponent } from './components/acad-history-input/acad-history-input.component';
+import { DisplayQualificationsComponent } from './components/display-qualifications/display-qualifications.component';
+import { DisplayQualModulesComponent } from './components/display-qual-modules/display-qual-modules.component';
+
+import {AcademicRecordService} from "./services/academic-record.service";
+import {AcademicRecordModuleService} from "./services/academic-record-module.service";
+
+import '@webcomponents/custom-elements';
+import '@clr/icons';
+import '@clr/icons/shapes/all-shapes';
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import {MyadminLibModule, HttpErrorInterceptor} from "myadmin-lib";
+/** Http interceptor providers in outside-in order */
+export const HttpInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true }
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    AcadHistoryInputComponent,
+    DisplayQualificationsComponent,
+    DisplayQualModulesComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MyadminLibModule,
+    ClarityModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BlockUIModule.forRoot()
+  ],
+  providers: [AcademicRecordService, AcademicRecordModuleService, HttpInterceptorProviders],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ myadmin-angular/projects/academic-history-record-app/src/app/components/display-qual-modules/display-qual-modules.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'unisa-display-qual-modules',
+  templateUrl: './display-qual-modules.component.html',
+  styleUrls: ['./display-qual-modules.component.scss']
+})
+export class DisplayQualModulesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
