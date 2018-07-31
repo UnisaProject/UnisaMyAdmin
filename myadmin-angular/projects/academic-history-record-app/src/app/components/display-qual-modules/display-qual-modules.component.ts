5eeb4caf43568adaa346c293f888bb10761143dd
adrianhfoster <adrian@opencollab.co.za>
2018-07-31 17:31:39 +0200

    UMA-57 Academic Record App : Angular screens

--- myadmin-angular/projects/academic-history-record-app/src/app/components/display-qual-modules/display-qual-modules.component.ts
+++ myadmin-angular/projects/academic-history-record-app/src/app/components/display-qual-modules/display-qual-modules.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {BlockUI, NgBlockUI} from 'ng-block-ui';
+import {StudentInfo}from 'myadmin-lib';
+import {AcademicModuleResultInfo,AcademicQualResultInfo} from '../../info-objects';
 
 @Component({
   selector: 'unisa-display-qual-modules',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplayQualModulesComponent implements OnInit {
 
+  creditsOnly:string='N';
+
+  isStudent :boolean=false;
+
+  public studentInfo: StudentInfo;
+
+  public academicModuleRecords: AcademicModuleResultInfo[] = [];
+
+  public academicQualificationRecords: AcademicQualResultInfo;
+
+  @BlockUI()
+  private blockUI: NgBlockUI;
+
   constructor() { }
 
   ngOnInit() {
+    this.blockUI.stop();
   }
 
+  studentExists():boolean {
+    return this.studentInfo ==null;
+  }
 }
